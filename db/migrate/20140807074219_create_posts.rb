e2f8b3ccdad85d760574c4362f91a3bcf759f38e
Thorne Brandt <thornebrandt@gmail.com>
2014-08-07 04:29:35 -0500

    first commit

---
+++ db/migrate/20140807074219_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :content
+      t.integer :type
+
+      t.timestamps
+    end
+  end
+end
